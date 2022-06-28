1b3dd0de6850b72f792603e9de06036afa29fe57
Maria Eduarda <90484092+duda-amaral@users.noreply.github.com>
2022-06-28 19:21:37 -0300

    Update codigoFonte.c
    adicionei o cadastro de cliente :)

--- codigoFonte.c
+++ codigoFonte.c
@@ -8,9 +8,10 @@
 int retornaCodigoVeiculo();
 void cadastraVeiculo();
 
-struct cadastroCliente
+struct TClientes
 {
-    char cliente[50];
+    char nome[50];
+    int codigo;
     int ddd;
     int telefone;
     char rua[10];
@@ -19,7 +20,12 @@ struct cadastroCliente
     char complemento[20];
 };
 
-typedef struct cadastroCliente cliente;
+typedef struct TClientes cliente;
+
+int menuClientes();
+void cadastraClientes(FILE *fclientes);
+int localizaCliente(FILE *fclientes, int codigo);
+void listaClientes(FILE *fclientes);
 
 int main()
 {
@@ -182,7 +188,7 @@ void menuPrincipal()
     
     case 1:
         printf("|\n|\nVocê escolheu:\n| 1. Cadastro de cliente\n");
-        cadastrarCliente();
+        menuClientes();
         break;
 
     case 2:
@@ -304,48 +310,132 @@ int retornaCodigoVeiculo()
     }
 }
 
-void cadastrarCliente()
+int menuClientes()
 {
-    FILE *arquivoCadastroClientes;
-    cliente c;
-    printf("\tBem vindo ao cadastro de clientes!\n");
-    printf("\t----------------------------------\n");
-    printf("\n\tNome do cliente: ");
-    gets(c.cliente);
-    fflush(stdin);
-    printf("\t----------------------------------\n");
-    printf("\n\t\tTelefone\n");
-    printf("\n\tDDD: ");
-    scanf("%i", &c.ddd);
-    printf("\tNúmero: ");
-    scanf("%i", &c.telefone);
-    printf("\t----------------------------------\n");
-    printf("\n\t\tEndereço");
-    printf("\n\n\tRua: ");
-    fflush(stdin);
-    gets(c.rua);
-    fflush(stdin);
-    printf("\tBairro: ");
-    fflush(stdin);
-    gets(c.bairro);
-    fflush(stdin);
-    printf("\tNúmero: ");
-    scanf("%i", &c.numero);
-    printf("\tComplemento: ");
-    fflush(stdin);
-    gets(c.complemento);
-    fflush(stdin);
-    printf("\n\tCadastro realizado com sucesso!");
-
-    if ((arquivoCadastroClientes = fopen("arquivoCadastroClientes.txt", "a")) == NULL)
+    FILE *fclientes;
+    int op;
+    if ((fclientes = fopen("clientesCadastrados.dat", "r+b")) == NULL)
     {
-        printf("Erro na abertura do arquivo.");
+        printf("\tArquivo não existia... Criando arquivo...\n");
+
+        if ((fclientes = fopen("clientesCadastrados.dat", "w+b")) == NULL)
+        {
+            printf("Erro na criação do arquivo!");
+            exit(1);
+        }
+    }
+
+    do
+    {
+        printf("\n\tBem vindo ao cadastro de cliente!\n\n");
+        printf("\t1 - Cadastrar cliente\n");
+        printf("\t2 - Alterar cliente\n");
+        printf("\t3 - Listar clientes\n");
+        printf("\t0 - Sair do programa\n");
+        printf("\tEscolha: ");
+        scanf("%i", &op);
+        switch (op)
+        {
+        case 1:
+            printf("\n\tVocê escolheu cadastrar um cliente!\n\n");
+            cadastraClientes(fclientes);
+            break;
+
+        case 2:
+            printf("\n\tVocê escolheu alterar os dados do cliente!\n\n");
+            //alteraCliente(fclientes);
+            break;
+
+        case 3:
+            printf("\n\tVocê escolheu listar todos os clientes!\n\n");
+            listaClientes(fclientes);
+            system("pause");
+            break;
+        }
+    }
+
+    while (op != 0);
+    fclose(fclientes);
+    return 0;
+}
+
+int localizaCliente(FILE *fclientes, int codigo)
+{
+    int posicao = -1, achou = 0;
+    cliente c;
+    fseek(fclientes, 0, SEEK_SET);
+    fread(&c, sizeof(c), 1, fclientes);
+    while (!feof(fclientes) && !achou)
+    {
+        posicao++;
+
+        if (c.codigo == codigo)
+        {
+            achou = 1;
+        }
+        fread(&c, sizeof(c), 1, fclientes);
+    }
+
+    if (achou)
+    {
+        return posicao;
     }
     else
     {
-        fprintf(arquivoCadastroClientes, "\t------------------\n\t[Informações Gerais]\n\n\tNome do cliente: %s\n\n", c.cliente);
-        fprintf(arquivoCadastroClientes, "\t[Telefone]\n\n\tNúmero: (%i)%i\n\n", c.ddd, c.telefone);
-        fprintf(arquivoCadastroClientes, "\t[Endereço]\n\n\tRua: %s\n\tBairro: %s\n\tNúmero: %i\n\tComplemento: %s\n\n\n", c.rua, c.bairro, c.numero, c.complemento);
-        fclose(arquivoCadastroClientes);
+        return -1;
+    }
+}
+
+void cadastraClientes(FILE *fclientes)
+{
+    cliente c; 
+    int posicao;
+    printf("\n\tCódigo do cliente: ");
+    fflush(stdin);
+    scanf("%i", &c.codigo);
+    posicao = localizaCliente(fclientes, c.codigo);
+    if(posicao == -1)
+    {
+        printf("\tNome do cliente: ");
+        fflush(stdin);
+        gets(c.nome);
+        printf("\tDDD e Telefone: ");
+        fflush(stdin);
+        scanf("%i%i", &c.ddd, &c.telefone);
+        printf("\tRua: ");
+        fflush(stdin);
+        gets(c.rua);
+        printf("\tBairro: ");
+        fflush(stdin);
+        gets(c.bairro);
+        printf("\tNúmero: ");
+        fflush(stdin);
+        scanf("%i", &c.numero);
+        printf("\tComplemento: ");
+        fflush(stdin);
+        gets(c.complemento);
+        fseek(fclientes, 0, SEEK_END);
+        fwrite(&c, sizeof(c), 1, fclientes);
+        fflush(fclientes);
+    }
+    else
+    {
+        printf("\n\n\tO código %i já foi usado anteriomente. Cadastro não realizado.\n\tTente novamente!\n\n", c.codigo);
+    }
+}
+
+void listaClientes(FILE *fclientes)
+{
+    cliente c;
+    fseek(fclientes, 0, SEEK_SET);
+    fread(&c, sizeof(c), 1, fclientes);
+    while(!feof(fclientes))
+    {
+        printf("\n\n\t[Dados do cliente]\n");
+        printf("\tNome: %s\n", c.nome);
+        printf("\tCódigo (CPF): %i\n", c.codigo);
+        printf("\tTelefone: (%i)%i\n", c.ddd, c.telefone);
+        printf("\tRua: %s\n\tBairro: %s\n\tNúmero: %i\n\tComplemento: %s\n", c.rua, c.bairro, c.numero, c.complemento);
+        fread(&c, sizeof(c), 1, fclientes);
     }
 }
